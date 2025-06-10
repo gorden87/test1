a34b7e053760939e4169ab5335f4ca5569aececc
gorden87 <gorden87@gmail.com>
2025-06-10 10:40:27 +0700

    first commit

--- my-static-app/script.js
+++
@@ -1,9 +0,0 @@
-function updateTime() {
-  const now = new Date().toLocaleTimeString();
-  document.getElementById('timestamp').innerText = 'Текущее время: ' + now;
-}
-
-// Автообновление времени при загрузке
-window.onload = () => {
-  updateTime();
-};
